2e7f16039e83184ccebc42e853c95af10d296e45
Sivas6496 <79104462+Sivas6496@users.noreply.github.com>
2021-06-11 21:47:03 +0530

    Added HEader js and used inside App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./styles.css";
+import Header from "./Header";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox!</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Header />
+      <button>Add</button>
+      <div>
+        <span>Name</span>
+        <br />
+        <span>Phone</span>
+      </div>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const Header=function(){
+  return(
+    <div>
+      Phone Directory
+    </div>
+  )
+}
+
+export default Header;
