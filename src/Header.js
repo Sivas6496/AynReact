3fb88382910f6dc5324ac3b2fd3b72c143ef607e
Sivas6496 <79104462+Sivas6496@users.noreply.github.com>
2021-06-12 12:42:21 +0530

    Added External Style

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,6 @@
 import React from "react";
 const Header = function () {
-  const headerStyle = {
-    background: "black",
-    color: "white",
-    textAlign: "center",
-    textTransform: "uppercase",
-    padding: 20
-  };
-  return <div style={headerStyle}>Phone Directory</div>;
+  return <div className="header">Phone Directory</div>;
 };
 
 export default Header;
